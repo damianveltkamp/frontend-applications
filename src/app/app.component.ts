ce6a0f6060ff43cc78a2ac20d355f31c789971ef
Damian Veltkamp <damian@bitethelemon.nl>
2019-10-30 13:04:37 +0100

    Fixed GeoJson features on map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,5 @@ import { HttpClient } from '@angular/common/http';
 })
 ​
 export class AppComponent {
-    url = `https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-25/sparql?default-graph-uri=&query=++++++++++++++++++++PREFIX+dct%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0D%0A++++++++++++++++++++SELECT+*+WHERE+%7B%0D%0A++++++++++++++++++++%3Fsub+dct%3Acreated+%221990%22+.%0D%0A++++++++++++++++++++%7D+LIMIT+50&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on`
-    responseData: Object [];
-    constructor(private http: HttpClient) {
-        this.http.get(this.url).subscribe(response=> {
-            const responseData = response;
-            console.log(responseData)
-        })
-    }
-​
+    constructor(private http: HttpClient) {}
 }
--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,14 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import { HttpClient } from '@angular/common/http';
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
-});
-
-L.Marker.prototype.options.icon = DefaultIcon;
 @Component({
     selector: 'app-maps',
     templateUrl: './maps.component.html',
@@ -16,37 +9,54 @@ L.Marker.prototype.options.icon = DefaultIcon;
 })
 
 export class MapsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-      var mymap = L.map('mapid').setView([52.4307, 4.9139], 18);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-
-    let planes = [
-        {
-            lang:52.4327,
-            lat:4.91848,
-            popup:"<h1>Volk1</h1><br /><p>Description volk</p><br />'<a href='/volk1'>Bekijk nu</a>'"
-        },
-        {
-            lang:52.43122,
-            lat:4.91254,
-            popup:"<h1>Volk2</h1><br /><p>Description volk</p><br />'<a href='/volk2'>Bekijk nu</a>'"
-        }
-    ]
-
-    // Loop through markers
-    for(let i = 0;i < planes.length;i++) {
-        addMarker(planes[i]);
+    readonly GEOJSON_URL = './assets/data/geojson.json';
+    geojson = [];
+    constructor(private http: HttpClient) {
     }
 
-    function addMarker(props): void {
-        let marker = L.marker([props.lang,props.lat])
-        .bindPopup(props.popup)
-        .addTo(mymap);
+    ngOnInit() {
+        var map = L.map('mapid');
+        this.buildMap(map);
+        // Hier roep ik de functie aan die mijn data ophaalt, en geef ik een anonieme functie mee die wanneer alles
+        // uitgevoerd is pas zijn werk doet
+        this.getGeojson(() => {
+            console.log('als laatste');
+            // Loop over alle features in de geojson array
+            for(let i = 0; i < this.geojson[1].length; i++) {
+                this.addMapFeatures(map,this.geojson[1][i]);
+            }
+        });
     }
-  }
+
+    buildMap(map) {
+        map.setView([-8.3297, 115.1312], 10);
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    }
+
+    getGeojson(callback) {
+        this.http.get(this.GEOJSON_URL).toPromise().then(data => {
+            for (let key in data) {
+                if (data.hasOwnProperty(key)) {
+                    this.geojson.push(data[key]);
+                }
+            }
+            callback();
+        });
+    }
+
+    addMapFeatures(map,item) {
+        // Voeg de GeoJson items toe aan de map
+        L.geoJSON(item, {
+            // Voeg voor elke feature aan zijn layer een popup toe
+            onEachFeature: function (feature, layer) {
+                // Wanneer de feature geen popup content heeft, voeg geen popup toe
+                if(!feature.properties.popupTitle && !feature.properties.popupContent && !feature.properties.linkurl) {
+            } else {
+                layer.bindPopup("<h1>"+feature.properties.popupTitle+"</h1>"+"<p>"+feature.properties.popupContent+"</p>");
+            }
+            }
+        }).addTo(map);
+    }
 }
